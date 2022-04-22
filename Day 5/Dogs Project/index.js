9d0c3feac97e6833541115d30b289701a28510be
chtrli <chtrl2022@gmail.com>
2022-04-22 18:32:54 +0530

    day-6

--- Day 5/Dogs Project/index.js
+++ Day 5/Dogs Project/index.js
@@ -20,13 +20,32 @@ function createBreedList(breedList){
 
 async function loadByBreed(breed) {
     if(breed != "Choose a dog breed") {
-       const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+       const response = await fetch( `https://dog.ceo/api/breed/${breed}/images`)
        const data = await response.json();
        createSlideShow(data.message);
     }
 }
 
 
-function createSlideShow(imgMessage){
-    console.log(imgMessage);
+function createSlideShow(images){
+    let currPos = 0;
+    document.getElementById('slide').innerHTML = 
+    ` <div class="slideshow" style="background-image : url('${images[0]}')"></div>
+      <div class="slideshow" style="background-image : url('${images[1]}')"></div>`
+      currPos = currPos + 2;
+      setInterval(nextSlide , 3000)
+
+      function nextSlide(){
+          document.getElementById('slide').insertAdjacentElement("beforeend", `<div class="slideshow" style="background-image : url('${images[currPos]}')"></div>` )
+          setTimeout(function(){
+              document.querySelector("#slide").remove();
+          } ,1000)
+
+          if(currPos + 1 >= images.length){
+            currPos = 0;
+          }
+          else{
+                currPos++ ;
+          }
+      }
 }
